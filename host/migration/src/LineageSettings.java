e5ab4b9c1cb2dbc872c9cfd7ce7294792c3a5aae
bimuafaq <bimuafaq@duck.com>
2026-02-07 09:12:27 +0000

    BatteryMeter: Add OneUI style [1/2]
    Signed-off-by: bimuafaq <bimuafaq@duck.com>

--- host/migration/src/LineageSettings.java
+++ host/migration/src/LineageSettings.java
@@ -83,6 +83,7 @@ public final class LineageSettings {
          * 0: Display the battery an icon in portrait mode
          * 1: Display the battery as a circle
          * 2: Display the battery as plain text
+         * 3: Display the battery as OneUI
          * default: 0
          * @hide
          */
--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -974,7 +974,7 @@ public final class LineageSettings {
 
         /** @hide */
         public static final Validator STATUS_BAR_BATTERY_STYLE_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 2);
+                new InclusiveIntegerRangeValidator(0, 3);
 
         /**
          * Status bar battery %
